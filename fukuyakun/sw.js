d103e2accaebf8f742f1dae400a6d84467ac5b9c
wh77kmqp29 <wh77kmqp29@privaterelay.appleid.com>
2026-05-11 14:21:56 +0900

    Add service worker for caching and notifications

---
+++ fukuyakun/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'fukuyakun-v1';
+const ASSETS = ['./fukuyakun.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(cls => {
+      if (cls.length > 0) {
+        cls[0].focus();
+        return cls[0].navigate('./fukuyakun.html');
+      }
+      return clients.openWindow('./fukuyakun.html');
+    })
+  );
+});
